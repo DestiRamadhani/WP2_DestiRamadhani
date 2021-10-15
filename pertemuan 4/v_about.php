2a8bc8804213a65284a0d68d83ab41f998d39fc7
DestiRamadhani <91050255+DestiRamadhani@users.noreply.github.com>
2021-10-16 05:16:44 +0700

    Add files via upload

---
+++ pertemuan 4/v_about.php
@@ -0,0 +1,23 @@
+<section>     
+    <h1><?php echo $judul ?></h1> 
+  
+    <h4>Nama</h4>     
+    <ul type="disc">         
+        <li>Nama Depan : Desti</li>         
+        <li>Nama Belakang : Ramadhani</li>     
+    </ul>     
+    <br>     
+    <h4>Alamat</h4>     
+    <ul type="none">         
+        <li> Jalan Almujahidin No. 168 Kreo Selatan</li>     
+    </ul>          
+    <h4>Tempat Lahir</h4>    
+    <ul type="none">         
+        <li>Tangerang</li>     
+    </ul>  
+    <h4>Makanan Favorite</h4>     
+    <ul type="square">         
+        <li>Seblak</li>         
+        <li>Cakwe</li>     
+    </ul> 
+</section> 
